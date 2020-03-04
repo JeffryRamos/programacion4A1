7ce1071359228f215c0dfd74ef442a899013f0bf
JeffryRamos <jefryfranciscoramos@gmail.com>
2020-03-04 10:23:00 -0600

    JeffryRamos

--- clase java/app.js
+++ clase java/app.js
@@ -1,5 +1,5 @@
 document.addEventListener("DOMContentLoaded", e=>{
-    const form = document.querySelector("#frmConversores");
+    const form = document.querySelector("#frmConversoresMonedas");
     form.addEventListener("submit", event=>{
         event.preventDefault();
 
@@ -17,4 +17,23 @@ document.addEventListener("DOMContentLoaded", e=>{
         let $res = document.querySelector("#lblRespuesta");
         $res.innerHTML = `Respuesta: ${ (monedas[a]/monedas[de]*cantidad).toFixed(2) }`;
     });
+    const form = document.querySelector("#frmConversoresLongitud");
+    form.addEventListener("submit", event=>{
+        event.preventDefault();
+
+        let de = document.querySelector("#cboDe").value,
+            a = document.querySelector("#cboA").value,
+            cantidad = document.querySelector("#txtCantidadConversor").value;
+        console.log(de, a, cantidad);
+        let monedas = {
+            "dolar":1,
+            "centimetro":100,
+            "quilometro":0.001,
+            "milla":0.000621371,
+            "pies":3.2808388799999997
+        };
+        let $res = document.querySelector("#lblRespuesta");
+        $res.innerHTML = `Respuesta: ${ (monedas[a]/monedas[de]*cantidad).toFixed(2) }`;
+    });
+    
 });
