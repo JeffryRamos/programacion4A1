c0748dd1cf6861c1891eb6e390e3c30b6f5a4db2
24RCR28 <nayelipineda290716@gmail.com>
2020-06-17 21:05:46 -0600

    chat

--- public/vistas/chat/chat.js
+++ public/vistas/chat/chat.js
@@ -1,65 +1,39 @@
 var appchat = new Vue({
-    el: '#frm-chat',
-    data:{
-        msg: {
-            del: '',
-            para: 'administrador',
-            msg: '',
-            imagen:''
+    /**
+     * @property el element del DOM a enlazar.
+     */
+        el:'#frm-chat',
+        data:{
+            msg : '',
+            msgs : []
         },
-        msgs: []
-    },
-    methods : {
-        enviarMensjae() {
-            if (this.msg.msg.trim() != '') {
+        methods:{
+            /**
+             * @function enviarMensaje es cuando el usuario envia un mensaje a otro usuario.
+             */
+            enviarMensaje(){
                 socket.emit('enviarMensaje', this.msg);
-                this.msg.msg = '';
+                this.msg = '';
+            },
+            /**
+             * @function limpiarChat borra el texto que escribio el usuario en la caja de texto
+             */
+            limpiarChat(){
+                this.msg = '';
             }
         },
-        usuario() {
-            fetch(`private/Modulos/consultas/procesos.php?proceso=idLogin&consulta=""`).then(resp => resp.json()).then(resp => {
-                this.msg.de1 = resp[0].idLogin;
-                socket.emit('chatHistory');
-            });
-            this.finalChat();
-        },
-        cargarImagen(e) {
-            var file = e.target.files[0];
-            var reader = new FileReader();
-            reader.onload = (event) => {
-                this.msg.imagen = event.target.result
-                this.msg.msg ='';
-                socket.emit('enviarMensaje', this.msg);
-                this.msg.imagen = '';
-            };
-            reader.readAsDataURL(file);
-        },
-        finalChat() {
-            $("#scroll").animate({
-                scrollTop: $('#scroll')[0].scrollHeight
-            }, 1000);
-        }
-    },
-    created() {
-        this.usuario();
-    }
-});
-socket.on('recibirMensaje', msg => {
-    if (msg.de1 === appchat.msg.de1 && msg.para === appchat.msg.para ||
-        msg.para === appchat.msg.de1 && msg.de1 === appchat.msg.para) {
-        appchat.msgs.push(msg);
-        if (msg.de1 === appchat.msg.para) {
-            $.notification("Biblioteca Digital chat", msg.msg, 'img/logo.png');
-        }
-    }
-    appchat.finalChat();
-});
-socket.on('chatHistory', msgs => {
-    appchat.msgs = [];
-    msgs-forEach(item => {
-        if (item.de1 === appchat.msg.de1 && item.para === appchat.msg.para ||
-            item.para === appchat.msg.de1 && item.de1 === appchat.msg.para) {
-            appchat.msgs.push(item);
+        created(){
+            socket.emit('chatHistory');
         }
     });
-});
+    socket.on('recibirMensaje',msg=>{
+        console.log(msg);
+        $.notification("Biblioteca Digital",msg, 'https://image.winudf.com/v2/image1/Y29tLmZhY2Vib29rLm9yY2FfaWNvbl8xNTYxNDE0ODQ5XzAwMw/icon.png?w=100&fakeurl=1');
+        appchat.msgs.push(msg);
+    });
+    socket.on('chatHistory',msgs=>{
+        appchat.msgs = [];
+        msgs.forEach(item => {
+            appchat.msgs.push(item.msg);
+        });
+    });
