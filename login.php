7f5b4ad9841cf1ed0058fb5394460d6be16df510
kathiadinora <kathiadinora>
2020-06-20 15:27:56 -0600

    Login

--- login.php
+++ login.php
@@ -1,56 +1,39 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Iniciar Sesión</title>
-    <link rel="shortcut icon" href="imagenes/logolink.png"/>
-    <link rel="apple-touch-icon" href="imagenes/logolink.png">
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css" integrity="sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk" crossorigin="anonymous">
-    <link rel="stylesheet" href="public/css/estilos.css">
-    <link rel="stylesheet" href="public/alertifyjs/css/alertify.min.css">
-    <link rel="stylesheet" href="https://unpkg.com/vue-select@3.0.0/dist/vue-select.css">
-    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.0.8/css/solid.css">
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css">
+<?php session_start();
 
-</head>
-<body class="img-background">
-    <a href="index.php" class="btn btn-info col-md-1 text-light active" role="button" aria-pressed="true"><i class="fas fa-reply"></i> Volver</a>
-    <div class="modal-dialog text-center">
-        <div class="col-sm-8 main-section">
-            <div class="modal-content">
-                <div class=" col-12 user-img">
-                    <img src="public/img/usuario.png" alt="">
-                </div>
-                <div class="text-center bot">
-                    <h2>Iniciar Sesión</h2>
-                </div>
-                <form class="col-12 margin-title">
-                    <div class="form-group" id="user-group">
-                        <input type="text" class="form-control" placeholder="Nombre de usuario"/>
-                        <span class="msg-error"></span>
-                    </div>
-                    <div class="form-group" id="contrasena-group">
-                        <input type="password" class="form-control" placeholder="Contraseña"/>
-                        <span class="msg-error"></span>
-                    </div>
-                    <button type="button" class="btn btn-info"><i class="fas fa-sign-in-alt"></i> INGRESAR</button>
-                    <div class="dropdown-divider col-10"></div>
-                        <a class="dropdown-item" href="#">¿Se te olvidó tu contraseña?</a>
-                    </div>
-                    
-                </form>
-            </div>
-        </div>
-
-    </div>
+    $error = '';
     
+    if($_SERVER['REQUEST_METHOD'] == 'POST'){
+        
+        $usuario = $_POST['usuario'];
+        $password = $_POST['password'];
+        $password = hash('sha512', $password);
+        
+        try{
+            $conexion = new PDO('mysql:host=localhost;dbname=db_app_biblioteca', 'root', '');
+            }catch(PDOException $prueba_error){
+                echo "Error: " . $prueba_error->getMessage();
+            }
+        
+        $statement = $conexion->prepare('
+        SELECT * FROM registros WHERE usuario = :usuario AND password = :password'
+        );
+        
+        $statement->execute(array(
+            ':usuario' => $usuario,
+            ':password' => $password
+        ));
+            
+        $resultado = $statement->fetch();
+        
+        if ($resultado !== false){
+            $_SESSION['usuario'] = $usuario;
+            header('location: principal.html');
+        }else{
+            $error .= '<i>Este usuario no existe</i>';
+        }
+    }
+    
+require 'login/login-vista.php';
 
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/socket.io/2.3.0/socket.io.dev.js"></script>
-    <script src="https://use.fontawesome.com/releases/v5.0.7/js/all.js"></script>
-    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js"></script>
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
-    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.min.js" integrity="sha384-OgVRvuATP1z7JjHLkuOU7Xw704+h835Lr+6QL9UvYjZE3Ipu6Tp75j7Bh/kR0JKI" crossorigin="anonymous"></script>
-</body>
-</html>
+
+?>
--- registro.php
+++ registro.php
@@ -1,67 +1,62 @@
-<?php 
-    include 'private/login/cod_registro.php';
+<?php session_start();
+
+
+    
+    if ($_SERVER['REQUEST_METHOD'] == 'POST'){
+        
+        $nombre = $_POST['nombre'];
+        $email = $_POST['email'];
+        $usuario = $_POST['usuario'];
+        $password = $_POST['password'];
+        $password2 = $_POST['password2'];
+        
+        $password = hash('sha512', $password);
+        $password2 = hash('sha512', $password2);
+        
+        $error = '';
+        
+        if (empty($nombre) or empty($email) or empty($usuario) or empty($password) or empty($password2)){
+            
+            $error .= '<i>Favor de rellenar todos los campos</i>';
+        }else{
+            try{
+                $conexion = new PDO('mysql:host=localhost;dbname=db_app_biblioteca', 'root', '');
+            }catch(PDOException $prueba_error){
+                echo "Error: " . $prueba_error->getMessage();
+            }
+            
+            $statement = $conexion->prepare('SELECT * FROM registros WHERE usuario = :usuario LIMIT 1');
+            $statement->execute(array(':usuario' => $usuario));
+            $resultado = $statement->fetch();
+            
+                        
+            if ($resultado != false){
+                $error .= '<i>Este usuario ya existe</i>';
+            }
+            
+            if ($password != $password2){
+                $error .= '<i> Las contraseñas no coinciden</i>';
+            }
+            
+            
+        }
+        
+        if ($error == ''){
+            $statement = $conexion->prepare('INSERT INTO registros (idRegistro, nombre, email, usuario, password) VALUES (null, :nombre, :email, :usuario, :password)');
+            $statement->execute(array(
+                
+                ':nombre' => $nombre,
+                ':email' => $email,
+                ':usuario' => $usuario,
+                ':password' => $password
+                
+            ));
+            
+            $error .= '<i style="color: green;">Usuario registrado exitosamente</i>';
+        }
+    }
+
+
+    require 'login/registro-vista.php';
+
 ?>
-
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Registrarse</title>
-    <link rel="shortcut icon" href="imagenes/logolink.png"/>
-    <link rel="apple-touch-icon" href="imagenes/logolink.png">
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css" integrity="sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk" crossorigin="anonymous">
-    <link rel="stylesheet" href="public/css/estilos.css">
-    <link rel="stylesheet" href="public/alertifyjs/css/alertify.min.css">
-    <link rel="stylesheet" href="https://unpkg.com/vue-select@3.0.0/dist/vue-select.css">
-    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.0.8/css/solid.css">
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css">
-
-</head>
-<body class="img-background">
-    <a href="index.php" class="btn btn-info col-md-1 text-light active" role="button" aria-pressed="true"><i class="fas fa-reply"></i> Volver</a>
-    <div class="modal-dialog text-center">
-        <div class="col-sm-8 main-section">
-            <div class="modal-content">
-                <div class=" col-12 user-img">
-                    <img src="public/img/usuario.png" alt="">
-                </div>
-                <div class="text-center bot">
-                    <h2>REGISTRO</h2>
-                </div>
-                <form class="col-12 margin-title" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>" method="post">
-                    <div class="form-row">
-                        <div class="form-group col-md-6">
-                          <input type="text" class="form-control" placeholder="Nombre" name="NombreUser">
-                          <span class="msg-error"><?php echo $NombreUser_error?></span>
-                        </div>
-                        <div class="form-group col-md-6">
-                          <input type="text" class="form-control" placeholder="Apellido" name="ApellidoUser">
-                          <span class="msg-error"></span>
-                        </div>
-                        <div class="form-group">
-                            <input type="email" class="form-control" id="inputEmail4" placeholder="Correo" name="email">
-                            <span class="msg-error"></span>
-                        </div>
-                        <div class="form-group" id="user-group">
-                            <input type="text" class="form-control" placeholder="Nombre de usuario" name="username"/>
-                            <span class="msg-error"></span>
-                        </div>
-                        <div class="form-group" id="contrasena-group">
-                            <input type="password" class="form-control" placeholder="Contraseña" name="password"/>
-                            <span class="msg-error"></span>
-                        </div>
-                    </div>
-                    <button type="button" class="btn btn-info">REGISTRARSE</button>
-                    </div>
-                </form>
-            </div>
-        </div>
-    </div>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/socket.io/2.3.0/socket.io.dev.js"></script>
-    <script src="https://use.fontawesome.com/releases/v5.0.7/js/all.js"></script>
-    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js"></script>
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
-    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.min.js" integrity="sha384-OgVRvuATP1z7JjHLkuOU7Xw704+h835Lr+6QL9UvYjZE3Ipu6Tp75j7Bh/kR0JKI" crossorigin="anonymous"></script>
-</body>
-</html>
