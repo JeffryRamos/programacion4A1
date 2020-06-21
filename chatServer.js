988e87d9b8822128b341246eb9c6fd829998a394
24RCR28 <nayelipineda290716@gmail.com>
2020-06-21 16:29:18 -0600

    funciones de chat

--- chatServer.js
+++ chatServer.js
@@ -1,42 +1,30 @@
-//NOTA: Este archivo debe de ir en la carpeta de node -> C:\Program Files\nodejs
+
 var http = require('http').Server(),
     io   = require('socket.io')(http),
     MongoClient = require('mongodb').MongoClient,
     url  = 'mongodb://localhost:27017',
     dbName = 'chatPrueba';
 
-    const webpush = require('web-push'),
-        vapidKeys = {
-            publicKey:'BJHfdZQWnVUaHtWnSANmO46PAKlXzk6FnM2sYzSYxgjf-bNvNC8LHxwDrqi4Dt-LwbDyEB5K29sBX7PLRtOzB20',
-            privateKey:'QM1DCuNyxTIUSCtYbrT94WlbyREmfGVR0MVyj7_DUyQ'
-        };
-    var pushSubcriptions;//debe de almacenarse en una BD.
-    webpush.setVapidDetails("mailto:luishernandez@ugb.edu.sv",vapidKeys.publicKey, vapidKeys.privateKey);
-
-io.on('connection',socket=>{
-    socket.on('enviarMensaje',(msg)=>{
-        MongoClient.connect(url, (err,client)=>{
+io.on('connection', socket => {
+    socket.on('enviarMensaje', (msg) => {
+        MongoClient.connect(url, (err, client) => {
             const db = client.db(dbName);
-            db.collection('chat').insert({'user':'Luis', 'msg':msg});
-            io.emit('recibirMensaje',msg);
-            try {
-                const dataPush = JSON.stringify({
-                   title:'Notificacion PUSH desde el SERVIDOR',
-                   msg
-                });
-                console.log( "Endpoint: ", pushSubcriptions.endpoint );
-                webpush.sendNotification(pushSubcriptions,dataPush);
-            } catch (error) {
-                console.log("Error al intentar enviar la notificacion push", error);
-            }
+            db.collection('chat').insert({
+                'de1': msg.de1,
+                'para': msg.para,
+                'msg': msg.msg,
+                'imagen': msg.imagen
+            });
+            io.emit('recibirMensaje', msg);
+           
         });
     });
-    socket.on('chatHistory',()=>{
-        MongoClient.connect(url, (err, client)=>{
+    socket.on('chatHistory', () => {
+        MongoClient.connect(url, (err, client) => {
             const db = client.db(dbName);
-            db.collection('chat').find({}).toArray((err, msgs)=>{
-                io.emit('chatHistory',msgs);
-            }); 
+            db.collection('chat').find({}).toArray((err, msgs) => {
+                io.emit('chatHistory', msgs);
+            });
         });
     });
     socket.on("suscribirse",(subcriptions)=>{
@@ -44,6 +32,6 @@ io.on('connection',socket=>{
         console.log( pushSubcriptions.endpoint );
     });
 });
-http.listen(3001,()=>{
+http.listen(3001, () => {
     console.log('Escuchando peticiones por el puerto 3001, LISTO');
 });
